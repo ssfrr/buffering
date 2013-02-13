3e166a7492d42bbdefd49b60f685d7e8aaaaa8fd
Spencer Russell <spencer.f.russell@gmail.com>
2013-02-12 20:35:01 -0500

    adds wickihayden mapping to manta object

--- seq/manta.py
+++ seq/manta.py
@@ -1,5 +1,16 @@
 import liblo, sys
 
+
+wickihayden = [0,  2,  4,  6,  8,  10, 12, 14,
+               7,  9,  11, 13, 15, 17, 19, 21,
+               12, 14, 16, 18, 20, 22, 24, 26,
+               19, 21, 23, 25, 27, 29, 31, 33,
+               24, 26, 28, 30, 32, 34, 36, 38,
+               31, 33, 35, 37, 39, 41, 43, 45]
+
+def note_from_pad(pad_number):
+    return wickihayden[pad_number] + 30
+
 class Manta(object):
     pad_and_button = 'padandbutton'
     slider = 'slider'
@@ -39,6 +50,7 @@ class Manta(object):
 
     def set_led_pad(self, led_type, state):
         self.send_osc('/manta/led/pad', led_state, led_index)
+
 
 #"/manta/led/pad/row", "sii", LEDRowHandler, this);
 #"/manta/led/pad/column", "sii", LEDColumnHandler, this);
