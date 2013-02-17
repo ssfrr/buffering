2d5d1978daf7783c072103c2a894af20388ca366
Spencer Russell <spencer.f.russell@gmail.com>
2013-02-17 16:28:56 -0500

    mantaseq now sequencing LEDs properly

--- seq/mantaseq.py
+++ seq/mantaseq.py
@@ -4,22 +4,49 @@ from manta import (Manta,
                    PadVelocityEvent,
                    PadValueEvent,
                    note_from_pad,
-                   row_from_pad)
-from time import sleep
+                   row_from_pad,
+                   OFF, AMBER, RED,
+                   PAD_AND_BUTTON)
+import time
 
 class MantaSeq(object):
     def __init__(self):
         self._manta = Manta()
         self._midi_source = MIDISource('MantaSeq')
         self._seq = Seq()
+        self._manta.set_led_enable(PAD_AND_BUTTON, True)
+        self.led_color_threshold = 75
+        self.step_duration = 0.25
+        # the first step should get executed on the first process() call
+        self.next_step_timestamp = time.time()
+        self.current_step = 0
+
+    def _get_step_color(self, step_num):
+        if step_num == self.current_step:
+            return RED
+        elif self._seq.steps[step_num].velocity > 0:
+            return AMBER
+        else:
+            return OFF
 
     def process(self):
+        now = time.time()
         events = self._manta.process()
         for event in events:
             if isinstance(event, PadVelocityEvent):
                 self._process_pad_velocity_event(event)
             elif isinstance(event, PadValueEvent):
                 self._process_pad_value_event(event)
+        if now >= self.next_step_timestamp:
+            last_step = self.current_step
+            self.current_step = self._seq.current_step_index
+            self._seq.step()
+            self._manta.set_led_pad(self._get_step_color(last_step), last_step)
+            self._manta.set_led_pad(self._get_step_color(self.current_step),
+                                    self.current_step)
+            # remember which step we turned on so we can turn it off next time
+            # around
+            self.next_step_timestamp += self.step_duration
 
     def _process_pad_velocity_event(self, event):
         if row_from_pad(event.pad_num) < 2:
@@ -35,9 +62,23 @@ class MantaSeq(object):
                 self._midi_source.send(midi_note)
 
     def _process_pad_value_event(self, event):
-        note_num = note_from_pad(event.pad_num)
-        self._seq.set_note(note_num)
-        self._seq.set_velocity(event.value)
+        # pad value messages are ignored for step selection pads
+        if event.pad_num > 15:
+            note_num = note_from_pad(event.pad_num)
+            self._seq.set_note(note_num)
+            self._seq.set_velocity(event.value)
+            # first set the pad color of the note pad
+            if event.value == 0:
+                self._manta.set_led_pad(OFF, event.pad_num)
+            elif event.value < self.led_color_threshold:
+                self._manta.set_led_pad(AMBER, event.pad_num)
+            else:
+                self._manta.set_led_pad(RED, event.pad_num)
+
+            # then update the pad colors of any selected pads
+            for i, step in enumerate(self._seq.steps):
+                if step in self._seq.selected_steps:
+                    self._manta.set_led_pad(self._get_step_color(i), i)
 
 def make_note(note, velocity, channel = 0):
     return (0x90 | channel, note, velocity)
@@ -47,7 +88,7 @@ def main():
     try:
         while True:
             seq.process()
-            sleep(0.003)
+            time.sleep(0.001)
     except KeyboardInterrupt:
         pass
 
--- seq/test_mantaseq.py
+++ seq/test_mantaseq.py
@@ -2,7 +2,10 @@ import unittest
 from mock import Mock, patch
 from mantaseq import MantaSeq
 from mantaseq import make_note
-from manta import PadVelocityEvent, PadValueEvent
+from manta import (PadVelocityEvent,
+                   PadValueEvent,
+                   AMBER, RED, OFF,
+                   PAD_AND_BUTTON)
 
 # base MIDI note (for the first pad used for note selection)
 # is C3
@@ -16,13 +19,28 @@ class MockedBoundaryTest(unittest.TestCase):
         self.manta_patch.start()
         self.midi_patch = patch('mantaseq.MIDISource')
         self.midi_patch.start()
+        self.time_patch = patch('time.time',
+                side_effect = lambda: self.logical_time)
+        self.time_patch.start()
+        # allow the logical time of a test to be set
+        self.logical_time = 1000
+
         self.seq = MantaSeq()
         self.event_queue = []
         self.seq._manta.process.side_effect = self.get_next_event
+        self.seq._manta.set_led_pad.side_effect = self.set_led_state
+        self.led_states = [OFF] * 48
+
+    def step_time(self, amount):
+        self.logical_time += amount
 
     def tearDown(self):
         self.manta_patch.stop()
         self.midi_patch.stop()
+        self.time_patch.stop()
+
+    def set_led_state(self, led_state, pad_num):
+        self.led_states[pad_num] = led_state
 
     def get_next_event(self):
         # each element in event_queue should be a list of events to be
@@ -62,6 +80,8 @@ class MockedBoundaryTest(unittest.TestCase):
     def assert_no_midi_note_sent(self):
         self.assertEqual(len(self.seq._midi_source.send.mock_calls), 0)
 
+    def assert_led_state(self, pad_num, state):
+        self.assertEqual(self.led_states[pad_num], state)
 
 class TestPassThrough(MockedBoundaryTest):
     def test_process_generates_base_midi_note(self):
@@ -107,4 +127,71 @@ class TestStepSetting(MockedBoundaryTest):
 
     def test_pad_values_set_step_velocity(self):
         self.assertEqual(self.seq._seq.steps[3].velocity, 100)
+
+class TestLEDBehavior(MockedBoundaryTest):
+    def test_initializes_leds(self):
+        self.seq._manta.set_led_enable.assert_called_with(PAD_AND_BUTTON, True)
+
+    def test_low_pad_value_causes_amber_led(self):
+        self.enqueue_note_value_event(0, 35)
+        self.process_queued_manta_events()
+        self.assert_led_state(16, AMBER)
+
+    def test_high_pad_value_causes_red_led(self):
+        self.enqueue_note_value_event(0, 100)
+        self.process_queued_manta_events()
+        self.assert_led_state(16, RED)
+
+    def test_zero_pad_value_turns_led_off(self):
+        self.enqueue_note_value_event(0, 100)
+        self.enqueue_note_value_event(0, 0)
+        self.process_queued_manta_events()
+        self.assert_led_state(16, OFF)
+
+    def test_pad_values_dont_effect_step_pads(self):
+        self.event_queue.append(PadValueEvent(15, 50))
+        self.process_queued_manta_events()
+        self.assert_led_state(15, OFF)
+
+# changed desired behavior - step LEDs are only lit if there's an active
+# step there
+#    def test_selected_steps_are_lit_amber(self):
+#        self.enqueue_step_select(4)
+#        self.process_queued_manta_events()
+#        self.assert_led_state(4, AMBER)
+#
+#    def test_deselected_steps_are_off(self):
+#        self.enqueue_step_select(4)
+#        self.enqueue_step_deselect(4)
+#        self.process_queued_manta_events()
+#        self.assert_led_state(4, OFF)
+
+class TestStepping(MockedBoundaryTest):
+    def test_next_step_timestamp_should_be_incremented_on_first_process(self):
+        self.assertEqual(self.seq.next_step_timestamp, self.logical_time)
+        self.seq.process()
+        self.assertEqual(self.seq.next_step_timestamp,
+                self.logical_time + self.seq.step_duration)
+
+    def test_red_led_should_track_step(self):
+        self.seq.process()
+        self.assert_led_state(0, RED)
+        self.step_time(self.seq.step_duration)
+        self.seq.process()
+        self.assert_led_state(0, OFF)
+        self.assert_led_state(1, RED)
+
+    def test_active_steps_should_be_colored_amber(self):
+        self.enqueue_step_select(4)
+        self.enqueue_note_value_event(10, 45)
+        self.process_queued_manta_events()
+        self.assert_led_state(4, AMBER)
+
+    def test_active_steps_should_be_colored_amber_after_release(self):
+        self.enqueue_step_select(4)
+        self.enqueue_note_value_event(10, 45)
+        self.enqueue_step_deselect(4)
+        self.enqueue_note_value_event(10, 0)
+        self.process_queued_manta_events()
+        self.assert_led_state(4, AMBER)
 
