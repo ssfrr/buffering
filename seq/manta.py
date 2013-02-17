0fc382262658d11b7facd727ebb7b3fe788c849d
Spencer Russell <spencer.f.russell@gmail.com>
2013-02-17 10:14:22 -0500

    switched over to pyosc from liblo

--- seq/manta.py
+++ seq/manta.py
@@ -1,4 +1,5 @@
-import liblo, sys
+from OSC import OSCClient, OSCServer, OSCMessage
+import sys
 
 
 wickihayden = [0,  2,  4,  6,  8,  10, 12, 14,
@@ -11,45 +12,51 @@ wickihayden = [0,  2,  4,  6,  8,  10, 12, 14,
 def note_from_pad(pad_number):
     return wickihayden[pad_number] + 30
 
-class Manta(object):
-    pad_and_button = 'padandbutton'
-    slider = 'slider'
-    button = 'button'
-    red = 'red'
-    amber = 'amber'
-    off = 'off'
+# define constants
+PAD_AND_BUTTON = 'padandbutton'
+SLIDER = 'slider'
+BUTTON = 'button'
+RED = 'red'
+AMBER = 'amber'
+OFF = 'off'
 
-    def __init__(self, receive_port=8000, send_port=8001, send_address='127.0.0.1'):
-        self.osc_server = liblo.Server(receive_port)
-        self.osc_target = liblo.Address(send_port)
+class Manta(object):
+
+    def __init__(self, receive_port=31416, send_port=31417, send_address='127.0.0.1'):
+        self.osc_client = OSCClient()
+        self.osc_server = OSCServer(('127.0.0.1', receive_port))
+        self.osc_client.connect(('127.0.0.1', send_port))
+        # set the osc server to time out after 100ms
+        self.osc_server.timeout = 0.1
 
     def process(self):
-        self.osc_server.recv(100)
+        self.osc_server.handle_request()
 
-    def set_pad_value_callback(self, callback, user_data):
-        self.osc_server.add_method('/manta/continuous/pad', None, callback, user_data)
+    def set_pad_value_callback(self, callback):
+        self.osc_server.addMsgHandler('/manta/continuous/pad', callback)
 
-    def set_slider_value_callback(self, callback, user_data):
-        self.osc_server.add_method('/manta/continuous/slider', None, callback, user_data)
+    def set_slider_value_callback(self, callback):
+        self.osc_server.addMsgHandler('/manta/continuous/slider', callback)
 
-    def set_button_value_callback(self, callback, user_data):
-        self.osc_server.add_method('/manta/continuous/button', None, callback, user_data)
+    def set_button_value_callback(self, callback):
+        self.osc_server.addMsgHandler('/manta/continuous/button', callback)
 
-    def set_pad_velocity_callback(self, callback, user_data):
-        self.osc_server.add_method('/manta/valocity/pad', None, callback, user_data)
+    def set_pad_velocity_callback(self, callback):
+        self.osc_server.addMsgHandler('/manta/valocity/pad', callback)
 
-    def set_button_velocity_callback(self, callback, user_data):
-        self.osc_server.add_method('/manta/velocity/button', None, callback, user_data)
+    def set_button_velocity_callback(self, callback):
+        self.osc_server.addMsgHandler('/manta/velocity/button', callback)
 
     def send_osc(self, path, *args):
-        #liblo.send(self.osc_target, *args)
-        liblo.send(self.osc_target, "/foo/message1", 123)
+        msg = OSCMessage(path)
+        msg.append(args)
+        self.osc_client.send(msg)
 
     def set_led_enable(self, led_type, enabled):
-        self.send_osc('/manta/ledcontrol', "padandbutton", 1)
+        self.send_osc('/manta/ledcontrol', led_type, 1 if enabled else 0)
 
-    def set_led_pad(self, led_type, state):
-        self.send_osc('/manta/led/pad', led_state, led_index)
+    def set_led_pad(self, led_state, pad_index):
+        self.send_osc('/manta/led/pad', led_state, pad_index)
 
 
 #"/manta/led/pad/row", "sii", LEDRowHandler, this);
@@ -60,16 +67,25 @@ class Manta(object):
 
 def main():
     manta = Manta()
-    manta.set_led_enable(Manta.pad_and_button, 1)
+    manta.set_led_enable(PAD_AND_BUTTON, 1)
+    def print_pad_value(path, tags, args, source):
+        print args
+
+    def default_handler(path, tags, args, source):
+        print "Default Handler Called"
+        print "path: " + path
+        print "args: " + str(args)
 
     count = 10
     led_on = False
+    manta.osc_server.addMsgHandler('default', default_handler)
+    manta.set_pad_value_callback(print_pad_value)
+    print manta.osc_server
     while True:
         manta.process()
         count -= 1
-        print count
         if not count:
-            set_led_pad(Manta.off if led_on else Manta.red, 10)
+            manta.set_led_pad(OFF if led_on else RED, 10)
             led_on = not led_on
             count = 10
 
